69606d05541b8582de91cebc990fc974472056ea
Luis E. Sanchez Jr <luchopro@Luiss-MacBook-Pro.local>
2023-01-03 16:15:27 -0500

    Added 'list' feature in order to make a 'GET' request on the 'dishes' resource.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,9 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res) {
+    res.json({ data: dishes });
+}
+module.exports = {
+ list,
+};
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const controller = require("./dishes.controller");
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
