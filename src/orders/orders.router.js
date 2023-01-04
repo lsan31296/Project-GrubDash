5e5a6acf5f7ebd159325ce06b6e3d033cabc5785
Luis E. Sanchez Jr <luchopro@Luiss-MacBook-Pro.local>
2023-01-03 19:33:12 -0500

    Added 'list' feature in order to make 'GET' request for 'orders' resource.

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,8 @@
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const controller = require("./orders.controller");
 const router = require("express").Router();
 
 // TODO: Implement the /orders routes needed to make the tests pass
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
