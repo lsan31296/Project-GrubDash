b75aef960a6aa1f3f16ff4f82da76ca1de6027dd
Luis E. Sanchez Jr <luchopro@Luiss-MacBook-Pro.local>
2023-01-03 19:13:48 -0500

    Added 'update' feature in order to make a 'PUT' request for the 'dishes' resource

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -10,6 +10,65 @@ const nextId = require("../utils/nextId");
 function list(req, res) {
     res.json({ data: dishes });
 }
+
+function bodyDataHas(propertyName) {
+    return function(req, res, next) {
+        const { data = {} } = req.body;
+        if(data[propertyName]) {
+            return next();
+        }
+        next({ status: 400, message: `Must include a ${propertyName}`});
+    };
+}
+
+function dishExists(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find(dish => dish.id === dishId);
+    if (foundDish) {
+        res.locals.dish = foundDish;
+        return next();
+    }
+    next({ status: 404, message: `Dish does not exist: ${dishId}`});
+}
+function idPropertyIsValid(req, res, next) {
+    const { data: { id } = {} } = req.body;
+    const { dishId } = req.params;
+    if (id && dishId === id || !id) {
+        return next();
+    }
+    next({ status: 400, message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}` });
+}
+function pricePropertyIsValid(req, res, next) {
+    const { data: { price } = {} } = req.body;
+    if(price < 0 || !Number.isFinite(price)) {
+        return next({ status: 400, message: `price must be a valid number`});
+    }
+    next();
+}
+
+function update(req, res) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    res.locals.dish.name = name;
+    res.locals.dish.description = description;
+    res.locals.dish.price = price;
+    res.locals.dish.image_url = image_url;
+    res.json({ data: res.locals.dish });
+}
+
+function read(req, res) {
+    res.json({ data: res.locals.dish });
+}
 module.exports = {
- list,
+    list,
+    read: [dishExists, read],
+    update: [
+        dishExists, 
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        idPropertyIsValid,
+        pricePropertyIsValid, 
+        update
+    ],
 };
